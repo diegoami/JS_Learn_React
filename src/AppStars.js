c7cb179b32d9308d44731d3c589a15f70b11500b
Diego Amicabile <diego.amicabile@gmail.com>
2018-03-12 22:22:37 +0100

    Random amount of stars

--- src/AppStars.js
+++ src/AppStars.js
@@ -4,12 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome/styles.css';
 import './AppStars.css'
 const Stars = (props) => {
+    const numberOfStars = 1 + Math.floor(Math.random()*9);
+
+    let stars = [];
+    for (let i=0; i<numberOfStars; i++) {
+        stars.push(<i key={i} className="fa fa-star"></i>);
+    }
+
     return (
         <div className="col-5">
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
-            <i className="fa fa-star"></i>
+            {stars}
         </div>
     );
 };
