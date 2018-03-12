246c991001322ab513b04ebba3fbf78cb0ef7219
Diego Amicabile <diego.amicabile@gmail.com>
2018-03-12 22:18:59 +0100

    AppStars

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 //import App from './App';
 //import App from './Button2';
 //import { data, CardList} from './CardList'
-import AppCardList from './AppCardList';
+//import AppCardList from './AppCardList';
+import AppStars from './AppStars';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<AppCardList />, document.getElementById('root'));
+ReactDOM.render(<AppStars />, document.getElementById('root'));
 registerServiceWorker();
