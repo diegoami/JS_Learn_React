eba7d7ade36d502c3e2a0c6f8582850ba84388b6
Diego Amicabile <diego.amicabile@gmail.com>
2018-03-13 20:56:00 +0100

    App stars 2

--- src/AppStars.js
+++ src/AppStars.js
@@ -15,11 +15,32 @@ const Stars = (props) => {
 };
 
 const Button = (props) => {
+    let button;
+    switch(props.answerIsCorrect) {
+        case true:
+            button =
+                <button className="btn btn-success" onClick={props.acceptAnswer}>
+                    <i className="fa fa-check"></i>
+                </button>;
+            break;
+        case false:
+            button =
+                <button className="btn btn-danger">
+                    <i className="fa fa-times"></i>
+                </button>;
+            break;
+        default:
+            button =
+                <button className="btn"
+                        onClick={props.checkAnswer}
+                        disabled={props.selectedNumbers.length === 0}>
+                    =
+                </button>;
+            break;
+    }
     return (
         <div className="col-2">
-            <button className="btn" disabled={props.selectedNumbers.length === 0}>
-                =
-            </button>
+            {button}
         </div>
     );
 };
@@ -38,6 +59,9 @@ const Answer = (props) => {
 
 const Numbers = (props) => {
     const numberClassName = (number) => {
+        if (props.usedNumbers.indexOf(number) >= 0) {
+            return 'used';
+        }
         if (props.selectedNumbers.indexOf(number) >= 0) {
             return 'selected';
         }
@@ -62,34 +86,62 @@ class Game extends React.Component {
     state = {
         selectedNumbers: [],
         randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+        usedNumbers: [],
+        answerIsCorrect: null,
     };
     selectNumber = (clickedNumber) => {
         if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
         this.setState(prevState => ({
+            answerIsCorrect: null,
             selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
         }));
     };
     unselectNumber = (clickedNumber) => {
         this.setState(prevState => ({
+            answerIsCorrect: null,
             selectedNumbers: prevState.selectedNumbers
                 .filter(number => number !== clickedNumber)
         }));
     };
+    checkAnswer = () => {
+        this.setState(prevState => ({
+            answerIsCorrect: prevState.randomNumberOfStars ===
+            prevState.selectedNumbers.reduce((acc, n) => acc + n, 0)
+        }));
+    };
+    acceptAnswer = () => {
+        this.setState(prevState => ({
+            usedNumbers: prevState.usedNumbers.concat(prevState.selectedNumbers),
+            selectedNumbers: [],
+            answerIsCorrect: null,
+            randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+        }));
+    };
     render() {
-        const { selectedNumbers, randomNumberOfStars } = this.state;
+        const {
+            selectedNumbers,
+            randomNumberOfStars,
+            answerIsCorrect,
+            usedNumbers
+        } = this.state;
+
         return (
             <div className="container">
                 <h3>Play Nine</h3>
                 <hr />
                 <div className="row">
                     <Stars numberOfStars={randomNumberOfStars} />
-                    <Button selectedNumbers={selectedNumbers} />
+                    <Button selectedNumbers={selectedNumbers}
+                            checkAnswer={this.checkAnswer}
+                            acceptAnswer={this.acceptAnswer}
+                            answerIsCorrect={answerIsCorrect} />
                     <Answer selectedNumbers={selectedNumbers}
                             unselectNumber={this.unselectNumber} />
                 </div>
                 <br />
                 <Numbers selectedNumbers={selectedNumbers}
-                         selectNumber={this.selectNumber} />
+                         selectNumber={this.selectNumber}
+                         usedNumbers={usedNumbers} />
             </div>
         );
     }
